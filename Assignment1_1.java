cae9fac85268dd04925748b5f15cc81bffda07f7
Hekima <72218314+HekimaGit@users.noreply.github.com>
2022-05-11 22:41:08 -0500

    Add files via upload

---
+++ Assignment1_1.java
@@ -0,0 +1,5 @@
+public class Assignment1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hekima Muli\nJava\nOmaha, Nebraska\nCookies N Cream Ice Cream\n");
+    }
+}
---
+++ Assignment1_2.java
@@ -0,0 +1,6 @@
+public class Assignment1_2 {
+    public static void main(String[] args) {
+        // 29 chars per line
+        System.out.println("      J    A    V     V     A\n      J   A A    V   V     A A\nJ     J  AAAAA    V V     AAAAA\n J   J  A     A    V     A     A");
+    }
+}
